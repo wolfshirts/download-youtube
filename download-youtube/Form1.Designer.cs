b622f652d43d33d28b3ae6d7d4083ad8828be69f
wolfshirts <wolfshirts@tanana>
2018-11-12 16:35:49 -0800

    first commit

--- download-youtube/Form1.Designer.cs
+++ download-youtube/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.audioRadioButton = new System.Windows.Forms.RadioButton();
+            this.videoRadioButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(93, 2);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(418, 20);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Youtube URL:";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(435, 29);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 2;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // audioRadioButton
+            // 
+            this.audioRadioButton.AutoSize = true;
+            this.audioRadioButton.Location = new System.Drawing.Point(93, 29);
+            this.audioRadioButton.Name = "audioRadioButton";
+            this.audioRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.audioRadioButton.TabIndex = 3;
+            this.audioRadioButton.TabStop = true;
+            this.audioRadioButton.Text = "Audio";
+            this.audioRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // videoRadioButton
+            // 
+            this.videoRadioButton.AutoSize = true;
+            this.videoRadioButton.Location = new System.Drawing.Point(151, 29);
+            this.videoRadioButton.Name = "videoRadioButton";
+            this.videoRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.videoRadioButton.TabIndex = 4;
+            this.videoRadioButton.TabStop = true;
+            this.videoRadioButton.Text = "Video";
+            this.videoRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(531, 72);
+            this.Controls.Add(this.videoRadioButton);
+            this.Controls.Add(this.audioRadioButton);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.urlTextBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Youtube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.RadioButton audioRadioButton;
+        private System.Windows.Forms.RadioButton videoRadioButton;
     }
 }
 
--- download-youtube/Form1.cs
+++ download-youtube/Form1.cs
@@ -1,20 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using YoutubeExplode.Models.MediaStreams;
+using YoutubeExplode;
+
 
 namespace download_youtube
 {
     public partial class Form1 : Form
     {
+        enum RadioButtonState
+        {
+            audio,
+            video,
+            invalid
+        }
+
+        private RadioButtonState buttonSelected()
+        {
+            if (audioRadioButton.Checked)
+                return RadioButtonState.audio;
+            if (videoRadioButton.Checked)
+                return RadioButtonState.video;
+            return RadioButtonState.invalid;
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string saveFileLocation()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            if(save.ShowDialog() == DialogResult.OK)
+            {
+                return save.FileName.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        void invalidSaveError()
+        {
+            MessageBox.Show("Invalid save location.", "Invalid Save Error");
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            string saveLocation;
+
+            switch (buttonSelected())
+            {
+                case RadioButtonState.audio:
+                    saveLocation = saveFileLocation();
+                    if (saveLocation != null)
+                        getVideo(urlTextBox.Text, saveLocation, false);
+                    else
+                    {
+                        invalidSaveError();
+                        return;
+                    }
+                    break;
+                case RadioButtonState.video: 
+                    saveLocation = saveFileLocation();
+                    if (saveLocation != null)
+                        getVideo(urlTextBox.Text, saveLocation, true);
+                    else
+                    {
+                        invalidSaveError();
+                        return;
+                    }
+                    break;
+                case RadioButtonState.invalid: MessageBox.Show("You must select a format.", "Invalid Format Error"); return;
+                default:
+                    break;
+            }
+        }
+
+        private async void getVideo( string address, string saveLocation, bool video)
+        {
+            var client = new YoutubeClient();
+            var id = YoutubeClient.ParseVideoId(address);
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id);
+            var streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
+            var ext = streamInfo.Container.GetFileExtension();
+            if( video )
+                await client.DownloadMediaStreamAsync(streamInfo, saveLocation+"."+ext);
+            if (!video)
+            {
+                await client.DownloadMediaStreamAsync(streamInfo, saveLocation + "." + "mp3");
+               // var idMatch = 
+            }
         }
     }
 }
